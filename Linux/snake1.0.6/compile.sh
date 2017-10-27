ea74671bfe9a68f05395ad2c791c2e83be9d87fe
master-genius <1146040444@qq.com>
2017-10-27 22:04:26 +0800

    --

--- Linux/snake1.0.6/compile.sh
+++ Linux/snake1.0.6/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc -o level.o -c level.c
-gcc -o sfun.o -c sfun.c
-gcc -o main.o -c main.c
+gcc -o level.o -c level.c -O2
+gcc -o sfun.o -c sfun.c -O2
+gcc -o main.o -c main.c -O2
 gcc -o snake main.o sfun.o level.o -O2
 
