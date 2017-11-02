e3b1618897d358eb4b0f1d8869c8472726a81440
master-genius <1146040444@qq.com>
2017-11-02 11:20:17 +0800

    --

---
+++ network/time_sc_test/ts.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define PORT    12345
+
+#define HOST_LEN    256
+
+int main(int ac, char* av[])
+{
+    char*ad="127.0.0.1";
+    if(ac>1)
+        ad=av[1];
+    struct sockaddr_in addr;
+
+    struct hostent* hp;
+
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero((void*)&addr,sizeof(addr));
+
+    if(sock_fd<0)
+    {
+        perror("socket");
+        return -1;
+    }
+
+    char hostname[HOST_LEN];
+
+    gethostname(hostname,HOST_LEN);
+
+    //hp=gethostbyname(hostname);
+
+    //bcopy((void*)(*(hp->h_addr_list)), (void*)&addr.sin_addr,hp->h_length);
+
+    addr.sin_port=htons(PORT);
+    addr.sin_addr.s_addr=inet_addr(ad);
+    addr.sin_family=AF_INET;
+
+    unsigned char*b=(char*)(&addr.sin_addr.s_addr);
+
+    printf("%d %d.%d.%d.%d\n",PORT,*(b+0),*(b+1),*(b+2),*(b+3));
+
+
+    if(bind(sock_fd,(struct sockaddr*)&addr,sizeof(addr)))
+    {
+        perror("bind");
+        printf("errno code:%d\n",errno);
+        return -1;
+    }
+
+    if(listen(sock_fd,1))
+    {
+        perror("listen");
+        return -1;
+    }
+
+    int sock_id;
+    struct sockaddr_in user;
+    int pid=0;
+
+    time_t tm;
+
+    char*p=NULL;
+
+    int ulen=sizeof(user);
+
+    while(1)
+    {
+        sock_id=accept(sock_fd,(struct sockaddr*)&user,(socklen_t*)&ulen);
+        if(sock_id>0)
+        {
+            b=(char*)&user.sin_addr.s_addr;
+            printf("get call from %d.%d.%d.%d\n",*(b),*(b+1),*(b+2),*(b+3));
+        }
+        else
+            continue;
+        pid=fork();
+        if(pid>0)
+        {
+            close(sock_id);
+        }
+        else if(pid==0)
+        {
+            int pid = getpid();
+            close(sock_fd);
+          next_time:;
+            tm=time(0);
+            p=ctime(&tm);
+            if(send(sock_id,(void*)p,strlen(p),0)==-1)
+            {
+                perror("send");
+                close(sock_id);
+                return -1;
+            }
+            printf("process %d send success -- time: %ld\n",pid,tm);
+            //close(sock_id);
+            sleep(1);
+            goto next_time;
+        }
+        else if(pid<0)
+        {
+            perror("fork");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
